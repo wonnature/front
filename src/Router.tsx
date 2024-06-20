9ac2770abceb9cadd1bf2d754009abc172cab0a6
leeyeonju02 <dlduswn0783@naver.com>
2024-06-21 00:55:53 +0900

    feat: 인사말

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,11 @@ import Sidebar from "./assets/components/Sidebar";
 import Header from "./assets/components/Header";
 
 const CompanyIntro = lazy(() => import("./pages/intro/CompanyIntro"));
-
+const Greeting = lazy(() => import("./pages/intro/Greeting"));
+const Purpose = lazy(() => import("./pages/intro/Purpose"));
+const History = lazy(() => import("./pages/intro/History"));
+const Business = lazy(() => import("./pages/intro/Business"));
+const Map = lazy(() => import("./pages/intro/Map"));
 const Router = () => {
   return (
     <BrowserRouter>
@@ -13,6 +17,11 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<div>HOME </div>} />
         <Route path="/introduce/intro" element={<CompanyIntro />} />
+        <Route path="/introduce/greeting" element={<Greeting />} />
+        <Route path="/introduce/purpose" element={<Purpose />} />
+        <Route path="/introduce/history" element={<History />} />
+        <Route path="/introduce/business-field" element={<Business />} />
+        <Route path="/introduce/map" element={<Map />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/intro/CompanyIntro.tsx
+++ src/pages/intro/CompanyIntro.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import companyIntroData from "../../../public/intro-list.json";
 
